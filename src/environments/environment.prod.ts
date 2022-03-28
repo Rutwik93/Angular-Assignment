aaca7eff34f1e66b4c7076c92400f72f6788eb5f
Rutwik Patel <rutwik.patel@aimdek.com>
2022-03-28 22:27:57 +0530

    Removed Unneccesary Files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDyXV2d5zefNA7g2aTPL33U62P9b6dNBpI",
+    authDomain: "fir-demo-7951e.firebaseapp.com",
+    projectId: "fir-demo-7951e",
+    storageBucket: "fir-demo-7951e.appspot.com",
+    messagingSenderId: "31095432618",
+    appId: "1:31095432618:web:8885ca66edb78a2678b7d3"
+  }
 };
